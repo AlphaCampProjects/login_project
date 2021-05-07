956ebd1f9fcdd5a1c5fc3e082afa415fc8d6ba9f
tinahung126 <tina126hung@gmail.com>
2021-05-07 10:38:45 +0800

    feat:add mongoose

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+
+mongoose.connect("mongodb://localhost/login", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", () => {
+  console("mongodb error!");
+});
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }));
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log(`The express server is running on http://localhost:${port}`);
+});
